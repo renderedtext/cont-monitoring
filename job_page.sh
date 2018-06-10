12237a822bd86b7454b93171f9af771319e1de5b
Igor Šarčević <igor@renderedtext.com>
2018-06-10 19:21:34 +0200

    Display only errors

--- job_page.sh
+++ job_page.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-echo "==============================================================="
-echo "="
-echo "="
+set -x
 
 page_url="https://renderedtext.semaphoreci.com/jobs/$SEMAPHORE_JOB_ID"
 
-echo "$page_url"
-
-echo "Opening Job Page"
-curl -i -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE_VALUE" "$page_url" > /dev/null
-
-echo "Fetching the logs"
-curl -i -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE_VALUE" "$page_url/logs" > /dev/null
+curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE_VALUE" "$page_url" -o /dev/null
+curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE_VALUE" "$page_url/logs" -o /dev/null
