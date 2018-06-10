67ef22eead3aa2803509febfc08b5ef840861ff9
Igor Šarčević <igor@renderedtext.com>
2018-06-10 19:17:08 +0200

    Hide output

--- job_page.sh
+++ job_page.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 echo "==============================================================="
-echo ""
-echo ""
+echo "="
+echo "="
 
 page_url="https://renderedtext.semaphoreci.com/jobs/$SEMAPHORE_JOB_ID"
 
+echo "$page_url"
+
 echo "Opening Job Page"
-curl -i -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE_VALUE" "$page_url"
+curl -i -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE_VALUE" "$page_url" > /dev/null
 
 echo "Fetching the logs"
-curl -i -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE_VALUE" "$page_url/logs"
+curl -i -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE_VALUE" "$page_url/logs" > /dev/null
