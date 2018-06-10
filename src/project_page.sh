1cca24732eb0007207941d8d21c2fcd85e47ca9c
Igor Šarčević <igor@renderedtext.com>
2018-06-10 20:28:22 +0200

    Visit project page

--- src/project_page.sh
+++ src/project_page.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "---------------------------------"
-echo "Branch Page"
+echo "Project Page"
 echo "---------------------------------"
 
-page_url="https://renderedtext.semaphoreci.com/branches/$SEMAPHORE_BRANCH_ID"
+page_url="https://renderedtext.semaphoreci.com/projects/$SEMAPHORE_PROJECT_ID"
 
 set -x
 curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE_VALUE" "$page_url" -o /dev/null
