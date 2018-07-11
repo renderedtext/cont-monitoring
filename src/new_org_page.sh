2eec0f6221e0a6b1e98fe4603b6d643f15357678
Igor Šarčević <igor@renderedtext.com>
2018-07-11 21:52:20 +0200

    Visit new org pages

---
+++ src/new_org_page.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "---------------------------------"
+echo "New Org Page"
+echo "---------------------------------"
+
+page_url="https://me.semaphoreci.com/organizations/new"
+
+set -x
+curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE_VALUE" "$page_url" -o /dev/null
