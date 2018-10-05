fd85c741a83879154c319bc4f67b94b080dabf7a
Milica Nerlović <milica.nerlovic@gmail.com>
2018-10-05 15:59:30 +0200

    Visit more pages less frequently
    Visit project list and account pages. Also sleep a minute after each
    round of visits.

--- src/me_pages.sh
+++ src/me_pages.sh
@@ -8,3 +8,4 @@ page_url="https://me.semaphoreci.com"
 
 set -x
 curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE" "$page_url" -o /dev/null
+curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE" "$page_url/account" -o /dev/null
--- src/project_pages.sh
+++ src/project_pages.sh
@@ -4,7 +4,8 @@ echo "---------------------------------"
 echo "Project Page"
 echo "---------------------------------"
 
-page_url="https://semaphore.semaphoreci.com/projects/cont-monitoring"
+page_url="https://semaphore.semaphoreci.com/projects/"
 
 set -x
 curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE" "$page_url" -o /dev/null
+curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE" "$page_url/cont-monitoring" -o /dev/null
