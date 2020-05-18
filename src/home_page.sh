6b85bf6f3b7b7c765e9837611cdbddef8ac4efda
Marko Bogdanović <mamaveb@gmail.com>
2020-05-18 22:49:19 +0200

    Update home_page.sh

--- src/home_page.sh
+++ src/home_page.sh
@@ -8,4 +8,4 @@ page_url="https://semaphore.semaphoreci.com/"
 
 set -x
 curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE" "$page_url" -o /dev/null
-curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE" "$page_url/poll" -o /dev/null
+curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE" "$page_url/workflows" -o /dev/null
