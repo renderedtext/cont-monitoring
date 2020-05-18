11747aaf16d1677c5217a394c86da7588f4f60d8
Marko Bogdanović <mamaveb@gmail.com>
2020-05-18 16:24:23 +0200

    Update branch_page.sh

--- src/branch_page.sh
+++ src/branch_page.sh
@@ -4,7 +4,7 @@ echo "---------------------------------"
 echo "Branch Page"
 echo "---------------------------------"
 
-page_url="https://semaphore.semaphoreci.com/branches/7cc735cc-1cdb-4f92-8215-3afe3cee1f52"
+page_url="https://semaphore.semaphoreci.com/branches/d06123e9-a3bb-4941-901c-8c607c753573"
 
 set -x
 curl -sSL -D - -H "Cookie: $SESSION_COOKIE_NAME=$SESSION_COOKIE" "$page_url" -o /dev/null
